afa4a491e123087a5142c6d0ca892798e642794b
Scott Luse <scottwluse@gmail.com>
2018-07-28 20:05:18 -0700

    Update for Github user events

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
+# -------------------------------------------------#
+# Title: github-latest.py
+# Dev: Scott Luse
+# Date: 07/28/2018
+# -------------------------------------------------#
+
 import sys
 import json
 
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
+def github(user):
+    print("Retrieving event list for: " + user)
+
+    '''
+    https://api.github.com/users/{user}/repos{?type,page,per_page,sort}",
+    
+    1. Retrieve a list of "events" associated with the given user name
+    2. Print out the time stamp associated with the first event in that list.
+    
+    '''
+    response = requests.get("https://api.github.com/users/{}/events".format(user))
+    mod_date = response.json()[0]['created_at']
+    print(mod_date)
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
+    github(username)
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
-    
 
 
